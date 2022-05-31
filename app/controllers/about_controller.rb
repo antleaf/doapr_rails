f55788e995020726d49dd98ef3342263d020b59b
paulwalk <paul@paulwalk.net>
2022-05-31 14:55:39 +0100

    tidied forms, fixed labels

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,7 +1,6 @@
 class AboutController < ApplicationController
 
   def index
-    authorize self
-    # require_user!
+
   end
 end
--- app/policies/about_controller_policy.rb
+++ app/policies/about_controller_policy.rb
@@ -1,13 +1,13 @@
 class AboutControllerPolicy < ApplicationPolicy
-  attr_reader :user, :ctrlr
-
-  def initialize(user, ctrlr)
-    @user = user
-    @ctrlr = ctrlr
-  end
-
-  def index?
-    @user != nil && @user.has_role?(:admin)
-  end
+  # attr_reader :user, :ctrlr
+  #
+  # def initialize(user, ctrlr)
+  #   @user = user
+  #   @ctrlr = ctrlr
+  # end
+  #
+  # def index?
+  #   @user != nil && @user.has_role?(:admin)
+  # end
 
 end
